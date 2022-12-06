65c324240c281716f8ea0bce47e672031779c091
NatalyJessica <106627940+NatalyJessica@users.noreply.github.com>
2022-12-06 11:49:07 -0300

    lib

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:projetof/Principal.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: " ProjetoFinal ",
+      home: mainPrincipal()));
+}
